1207ee1a56ebe7c13456c49484bca1c02a45298b
pavan <pavankumar49210@gmail.com>
2026-07-05 19:24:13 +0530

    added

--- mysql.sh
+++ mysql.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 LOGS_FOLDER="/var/log/expense-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S)
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 ############################################
